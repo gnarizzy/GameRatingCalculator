67b45b3ad5c9117954d14904f80d6a6c13420c39
unknown <schua@1DT-SCHUA.alston.com>
2013-10-15 13:13:55 -0400

    Calculator now compatible with Profile class

--- src/com/Centaurii/app/RatingCalculator/util/Calculator.java
+++ src/com/Centaurii/app/RatingCalculator/util/Calculator.java
@@ -1,5 +1,7 @@
 package com.Centaurii.app.RatingCalculator.util;
 
+import com.Centaurii.app.RatingCalculator.model.Profile;
+
 /**
  * Model class that stores information of players and calculates new ratings
  * Created 1/15/12
@@ -9,101 +11,78 @@ package com.Centaurii.app.RatingCalculator.util;
  */
 public class Calculator
 {
-
-    private double rating1, rating2, rating3, rating4, rating5, rating6;
-    double[] ratings = { rating1, rating2, rating3, rating4, rating5, rating6 };
-    private final double KVALUE = 32;
-    private double size = 6.0;
-    private boolean player1, player2, player3, player4, player5, player6;
-    boolean[] provisionals = { player1, player2, player3, player4, player5,
-            player6 };
-    int winner = 0;
-
-    public void setWinner(int win)
-    {
-        winner = win;
-    }
-
-    public void setSize(int newSize)
-    {
-        size = (double) newSize;
-    }
-
-    public void setRating(int position, int rating)
-    {
-        ratings[position - 1] = (double) rating;
-    }
-
-    private double getAverageRating(int position)
-    {
-        double sum = 0.0;
-        for (int i = 0; i < (int) (size); i++)
-            sum = sum + ratings[i];
-        double total = sum - ratings[position - 1];
-        return total / (size - 1);
-
-    }
-
-    private double winExpectancy(double rating, double oppRating)
-
-    {
-        double exponent = -1 * (rating - oppRating) / 400.0;
-        double denominator = Math.pow(10, exponent) + 1;
-        double expectancy = (1.0 / denominator) * (2.0 / size);
-        // System.out.println("Expected score " + 1.0 / denominator);
-        // System.out.println("Expectancy is now " + expectancy);
-        return expectancy;
-    }
-
-    public void setProvisional(int position)
-    {
-        provisionals[position - 1] = true;
-    }
-
-    public void unSetProvisional(int position)
-    {
-        provisionals[position - 1] = false;
-    }
-
-    public int getSize()
-    {
-        return (int) (size);
-    }
-
-    public int[] calculateRating()
-    {
-        int[] results = new int[(int) size];
-        for (int i = 0; i < size; i++)
+    private final static double KVALUE = 32;
+    
+    public static int[] calculateRating(int winner, boolean commit, Profile... players)
+    {   
+        int tracker = 0;
+        int numberOfPlayers = players.length;
+        int[] endScores = new int[numberOfPlayers];
+        for(Profile player: players)
         {
-            if (winner == i)
+            //losing case
+            if(winner != tracker)
             {
-                if (provisionals[i])
-                    results[i] = (int) (Math.round((ratings[i] + 2
-                            * KVALUE
-                            * (1 - winExpectancy(ratings[i],
-                                    getAverageRating(i + 1))))));
-                else
-                    results[i] = (int) (Math.round((ratings[i] + KVALUE
-                            * (1 - winExpectancy(ratings[i],
-                                    getAverageRating(i + 1))))));
+                endScores[tracker] = (int) Math.round(player.getRating() + 
+                        calcRatingHelperLose(player, numberOfPlayers, calcAverage(players, tracker)));
+                if(commit)
+                {
+                    player.setRating(endScores[tracker]);
+                }
             }
+            //winning case
             else
             {
-                if (provisionals[i])
-                    results[i] = (int) (Math.round((ratings[i] + 2
-                            * KVALUE
-                            * (0 - winExpectancy(ratings[i],
-                                    getAverageRating(i + 1))))));
-                else
-                    results[i] = (int) (Math.round((ratings[i] + KVALUE
-                            * (0 - winExpectancy(ratings[i],
-                                    getAverageRating(i + 1))))));
-
+                endScores[tracker] = (int) Math.round(player.getRating() +
+                        calcRatingHelperWin(player, numberOfPlayers, calcAverage(players, tracker)));
+                if(commit)
+                {
+                    player.setRating(endScores[tracker]);
+                }
             }
+            tracker++;
         }
-
-        return results;
-
+        
+        return endScores;
     }
-
+    
+    private static int calcAverage(Profile[] players, int pos)
+    {
+        int avgRating = 0;
+        for(Profile player: players)
+        {
+            avgRating += player.getRating();
+        }
+        avgRating -= players[pos].getRating();
+        avgRating = avgRating / (players.length - 1);
+        return avgRating;
+    }
+    
+    private static double calcRatingHelperWin(Profile player, int numPlayers, int avgRating)
+    {
+        int rating = player.getRating();
+        
+        double exponent = -1 * (rating - avgRating) / 400.0;
+        double denominator = Math.pow(10, exponent) + 1;
+        double expectancy = (1.0 / denominator) * (2.0 / numPlayers);
+        if(player.isProvisional())
+        {
+            return (2 * KVALUE * (1 - expectancy));
+        }
+        return (KVALUE * (1 - expectancy));
+    }
+    
+    private static double calcRatingHelperLose(Profile player, int numPlayers, int avgRating)
+    {
+        int rating = player.getRating();
+        
+        double exponent = -1 * (rating - avgRating) / 400.0;
+        double denominator = Math.pow(10, exponent) + 1;
+        double expectancy = (1.0 / denominator) * (2.0 / numPlayers);
+        if(player.isProvisional())
+        {
+            return (2 * KVALUE * (0 - expectancy));
+        }
+        return (KVALUE * (0 - expectancy));
+    }
 }
